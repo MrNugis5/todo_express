beb1bd2c1e3e6eba91ab18e40d187416fb664f7a
Martin <Martin.nugis@voco.ee>
2026-09-08 11:28:09 +0300

    Write data to file

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 const express = require("express")
 const app = express()
+const fs = require("fs");
 
 const path = require("path")
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
 
+const readFile = (filePath) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, "utf-8", (err, data) => {
+            if (err) {
+                console.error(err)
+                return;
+            }
+            const tasks = data.split("\n")
+            resolve(tasks)
+        })
+    })
+}
 app.get("/", (req, res) => {
-    res.render("index")
-} )
+    readFile("./tasks").then((tasks) => {
+        console.log(tasks)
+        res.render("index", { tasks: tasks })
+    })
+})
 
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/", (req, res) => {
+    readFile("./tasks").then((tasks) => {
+        tasks.push(req.body.task)
+        const data = tasks.join("\n")
+        fs.writeFile("./tasks", data, (err) => {
+            if (err) {
+                console.error(err)
+                return;
+            }
+            res.redirect("/")
+        })
+})
+})
 
 app.listen(3001, () =>{
     console.log("Example app is started at http://localhost:3001")
-} )
+} )  
